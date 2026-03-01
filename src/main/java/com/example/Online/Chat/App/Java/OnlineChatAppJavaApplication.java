8f80636984a145362bf0be97d181d69dd1927b46
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-01 19:15:46 +0530

    create spring structure with maven using spring initializer.

---
+++ src/main/java/com/example/Online/Chat/App/Java/OnlineChatAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Online.Chat.App.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineChatAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Online/Chat/App/Java/OnlineChatAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Online.Chat.App.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
