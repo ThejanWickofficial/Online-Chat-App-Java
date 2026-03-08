0903765e4bbabb54777ac754d69f5763072a3784
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-08 15:16:12 +0530

    call stompFrameHandler function from spring

--- src/main/java/com/example/Online/Chat/App/Java/client/MyStompSessionHandler.java
+++ src/main/java/com/example/Online/Chat/App/Java/client/MyStompSessionHandler.java
@@ -1,13 +1,27 @@
 package com.example.Online.Chat.App.Java.client;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
+import java.lang.reflect.Type;
+
 public class MyStompSessionHandler extends StompSessionHandlerAdapter {
     @Override
     public void afterConnected(StompSession session, StompHeaders connectHeaders){
+        session.subscribe("/topic/message", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return null;
+            }
 
+            @Override
+            public void handleFrame(StompHeaders headers, @Nullable Object payload) {
+
+            }
+        })
     }
 
     @Override
