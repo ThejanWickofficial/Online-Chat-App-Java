50f36b5ccb4240c350ce455a2f8aef06a1641ff4
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-08 23:54:37 +0530

    in WebSocketSessionManager.java file to manage users Use data structure as ArrayList

--- src/main/java/com/example/Online/Chat/App/Java/WebSocketSessionManager.java
+++ src/main/java/com/example/Online/Chat/App/Java/WebSocketSessionManager.java
@@ -1,8 +1,9 @@
 package com.example.Online.Chat.App.Java;
 
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
 @Service
 public class WebSocketSessionManager {
-
+    private final ArrayList<String> activeUserName =  new ArrayList<>();
 }
