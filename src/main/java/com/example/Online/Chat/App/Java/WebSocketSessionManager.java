872ceb74bcc45638c2e8fb972167423997b5d026
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-09 00:23:44 +0530

    in WebSocketSessionManager.java file create method to add and remove a username from the activeUsernames array

--- src/main/java/com/example/Online/Chat/App/Java/WebSocketSessionManager.java
+++ src/main/java/com/example/Online/Chat/App/Java/WebSocketSessionManager.java
@@ -1,9 +1,25 @@
 package com.example.Online.Chat.App.Java;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 
 @Service
 public class WebSocketSessionManager {
-    private final ArrayList<String> activeUserName =  new ArrayList<>();
+    private final ArrayList<String> activeUserNames =  new ArrayList<>();
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public WebSocketSessionManager(SimpMessagingTemplate messagingTemplate){
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void addUsername(String username){
+        activeUserNames.add(username);
+    }
+
+    public void removeUsername(String username){
+        activeUserNames.remove(username);
+    }
 }
