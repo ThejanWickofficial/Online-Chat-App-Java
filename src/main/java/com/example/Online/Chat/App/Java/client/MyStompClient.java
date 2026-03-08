6ec92f2c19207618924e6386becb7b23b5d05c2c
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-08 10:48:59 +0530

    create MyStompClient.java file

---
+++ src/main/java/com/example/Online/Chat/App/Java/client/MyStompClient.java
@@ -0,0 +1,28 @@
+package com.example.Online.Chat.App.Java.client;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStompClient {
+    private StompSession session;
+    private String username;
+
+    public MyStompClient(String username){
+        this.username = username;
+
+        List<Transport> tarnsports = new ArrayList<>();
+        tarnsports.add(new WebSocketTransport(new StandardWebSocketClient()));
+
+        SockJsClient sockJsClient = new SockJsClient(tarnsports);
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+    }
+}
