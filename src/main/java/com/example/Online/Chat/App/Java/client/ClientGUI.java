a441ff54978846ee54cd82c1f524b40b77faa55d
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-09 23:40:47 +0530

    Start creating GUI usin java.swing and Create App file to manage GUI and Backend

--- src/main/java/com/example/Online/Chat/App/Java/client/ClientGUI.java
+++ src/main/java/com/example/Online/Chat/App/Java/client/ClientGUI.java
@@ -3,14 +3,27 @@ package com.example.Online.Chat.App.Java.client;
 import com.example.Online.Chat.App.Java.Message;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.concurrent.ExecutionException;
 
 public class ClientGUI extends JFrame {
     public ClientGUI(String username){
         super("User: "+ username);
 
-        setSize(1218,685);
+        setSize(406,228);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int option = JOptionPane.showConfirmDialog(ClientGUI.this, "Do You really wan to close", "Exit",JOptionPane.YES_NO_OPTION);
+
+                if (option == JOptionPane.YES_OPTION){
+                    ClientGUI.this.dispose();
+                }
+            }
+        });
 
     }
 }
