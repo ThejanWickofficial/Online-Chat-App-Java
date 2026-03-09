1b1a67cf5fad3fb4a07b6867b38f816abcca0385
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-09 23:25:04 +0530

    Start creating GUI usin java.swing

--- src/main/java/com/example/Online/Chat/App/Java/client/ClientGUI.java
+++ src/main/java/com/example/Online/Chat/App/Java/client/ClientGUI.java
@@ -1,11 +1,16 @@
 package com.example.Online.Chat.App.Java.client;
 
 import com.example.Online.Chat.App.Java.Message;
+
+import javax.swing.*;
 import java.util.concurrent.ExecutionException;
 
-public class ClientGUI {
-    public static void main(String args[]) throws ExecutionException, InterruptedException {
-        MyStompClient myStompClient =  new MyStompClient("Taptap");
-        myStompClient.sendMessage(new Message("Taptap","Hello world"));
+public class ClientGUI extends JFrame {
+    public ClientGUI(String username){
+        super("User: "+ username);
+
+        setSize(1218,685);
+        setLocationRelativeTo(null);
+
     }
 }
