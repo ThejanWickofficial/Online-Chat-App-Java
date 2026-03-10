2161e842eca567dfd7b048badd02c7f87a01e46e
ThejanWickofficial <dt23thejan@gmail.com>
2026-03-10 19:26:14 +0530

    create Utilities.java file for the GUI

---
+++ src/main/java/com/example/Online/Chat/App/Java/client/Utilities.java
@@ -0,0 +1,9 @@
+package com.example.Online.Chat.App.Java.client;
+
+import java.awt.*;
+
+public class Utilities {
+    public static final Color TRANSPARENT_COLOR = new Color(0,0,0,0);
+    public static final Color PRIMARY_COLOR = new Color.decode("#2F2D2D");
+    public static final Color SECONDARY_COLOR = new Color.decode("#484444");
+}
